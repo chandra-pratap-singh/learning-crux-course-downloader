00f0f9288725928990a422033704e584e7c34f38
CP <chandrapratap321@gmail.com>
2020-08-09 12:31:08 +0530

    basic functionality done

---
+++ src/const.js
@@ -0,0 +1,6 @@
+const constants = {
+  URL: "https://www.learningcrux.com/course/advanced-javascript-concepts",
+  TIME_DELAY: 10000,
+};
+
+module.exports = constants;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+const puppeteer = require("puppeteer");
+const constants = require("./const.js");
+
+const URL = "https://www.learningcrux.com/course/advanced-javascript-concepts";
+const timeDelay = 10000;
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(constants.URL);
+  const videoUrls = await page.evaluate(() => {
+    addresses = [];
+    list = document.getElementsByClassName("panel-title fw-normal");
+    for (var i = 0; i < list.length; i++) {
+      addresses.push(list[i].children[0].href.replace("video", "play"));
+    }
+    return addresses;
+  });
+  console.log(videoUrls);
+  for (var i = 0; i < videoUrls.length; i++) {
+    try{
+      await page.goto(videoUrls[i]);
+    }catch{}
+    console.log('Downloading Video Number - ',i, ' of ',videoUrls.length);
+    await page.waitFor(constants.TIME_DELAY);
+  }
+})();
